baee02483222f13518e7e260fd3799ff9abb3294
IsaqueP <isaquepinheiro2002@gmail.com>
2023-01-19 21:13:45 -0300

    aula 3 finalizada

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -9,7 +9,7 @@ export function SummaryTable(){
 
   const minimumSummaryDatesSize = 18 * 7
   const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
-
+ 
   return(
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -25,7 +25,13 @@ export function SummaryTable(){
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay 
+              key={date.toString()} 
+              amount={5} 
+              completed={Math.round(Math.random() * 5)} 
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i)=>{
